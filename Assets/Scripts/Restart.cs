9a5e163a4552e4ca6971459b6131f21694a2ee94
erikort <erikort99@gmail.com>
2021-10-25 23:44:03 +0200

    25/10/2021

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    // Update is called once per frame
+    void RestartGame ()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
